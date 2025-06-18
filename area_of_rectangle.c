7f5e525f7c12d6c8dff4b3ff5c2bb718119e67f7
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-18 12:33:01 +0530

    snipet

---
+++ area_of_rectangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter side of rectangle :");
+    printf("enter another side of rectangle :");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    return 0;
+}
