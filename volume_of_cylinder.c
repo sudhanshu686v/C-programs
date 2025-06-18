bb09286adac021fbb432bc9aa043c88bc2d174cc
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-18 14:39:43 +0530

    volume of cylinder

---
+++ volume_of_cylinder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int r,h;
+
+    printf("enter radius :");
+    scanf("%d",&r);
+    printf("enter the height :");
+    scanf("%d",&h);
+    printf("volume of cylinder is :%f",3.14*r*r*h);
+    return 0;
+}
