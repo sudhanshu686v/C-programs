fad793ad82a49e2839c6134f0b73a877deca6490
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-18 11:05:49 +0530

    variable initialisation

---
+++ sizeof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=3;
+    float b=1.4; 
+    char c='a';
+    printf("size of int:%zu bytes\n",sizeof(int));
+    return 0;
+
+}
---
+++ variable.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a;//variable declaration
+    a=4; //variable initialisation
+    printf("the output of this prg is : %d",a);
+    return 0;
+}
