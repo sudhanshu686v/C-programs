8b91c030991f1c4ee9715344eeeebb6776eecfd0
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-19 18:00:16 +0530

    precidence and associativity

---
+++ associativity.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a=3;
+    int b=6; 
+    int c=9;
+    printf("the value is %d",a*b/c +7);
+    printf("\nthe value is %d",3*b/2*c +7*a); 
+    printf("\nthe value is %d",(3*b)/(2*c) +(7*a)); 
+    return 0;
+}
