e044a3d3af66a2972e9b18682d16f610210c07bf
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-20 03:25:01 +0530

    for loop

---
+++ for_loop.c
@@ -0,0 +1,13 @@
+//for (initailize;condition;update)
+
+#include <stdio.h>
+
+int main(){
+    int n=6,i;
+    for(i=0;i<n;i++){
+        printf("the value of i is %d\n",i);
+    }
+
+    
+    return 0;
+}
