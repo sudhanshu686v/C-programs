70c480e34de7e6433aae900be1c318aa78c76204
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-19 14:27:29 +0530

    initialisation and declaration

--- variable.c
+++ variable.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int main(){
-    int a;//variable declaration
-    a=4; //variable initialisation
-    printf("the output of this prg is : %d",a);
+    int i;//variable declaration
+    i=4; //variable initialisation
+    int j=i;
+    int a=4,b=6,c=8,d=2;
+    printf("the value of i is %d and the value of j is %d",i,j);
+    printf("\nthe value of a is %d and the value of d is %d",a,d);
+    printf("\nthe value of c is %d and the value of j is %d",c,j);
+    printf("\nthe value of b is : %d",b);
     return 0;
 }
