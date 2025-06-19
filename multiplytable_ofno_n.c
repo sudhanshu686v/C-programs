251a0b62af366e4f4dbc147efd893b82ae4072ca
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-20 04:03:19 +0530

    multiplication taple of number n

---
+++ multiplytable_ofno_n.c
@@ -0,0 +1,15 @@
+//prg to print multiplicatio table of given no. n
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("enter the no. ");
+    scanf("%d",&n);
+
+    for (int i = 1; i <=10; i++)
+    {
+        printf("%d\n",i*n);
+    }
+    
+    return 0;
+}
