85e5f49067f7b3771a9aa8f0fb29e18f76d7ab48
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-19 22:40:37 +0530

    ternary operator

---
+++ switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("enter a :");
+    scanf("%d",&a);
+
+    switch(a){
+        case 1:
+        printf("you entered 1\n");
+        break;
+        case 2:
+        printf("you entered 2\n");
+        break;
+        case 3:
+        printf("you entered 3\n");
+        break;
+        case 4:
+        printf("you entered 4\n");
+        break;
+        default :
+        printf("you entered nothing\n");
+    
+    }
+    return 0;
+
+}
