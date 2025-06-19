b231ef43bfc81a7cf5b7be25ad2ef456c770ebbe
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-20 04:48:18 +0530

    sum of first 10 numbers in multiplication table of 8

---
+++ sumofmultiplytableof8.c
@@ -0,0 +1,12 @@
+/*sum of numbers  in the mutiply table of 8*/
+#include <stdio.h>
+
+int main(){
+    int sum=0,table=1;
+    for(int i=1;i<=10;i++){
+        table=8*i;
+        sum+=table;
+    }
+    printf("sum of numbers  in the mutiply table of 8 is %d",sum);
+    return 0;
+}
