0dea499962e04d19ec0f3723f2e53d6abcd055c4
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-19 19:31:13 +0530

    relational operator

---
+++ reltional_operator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int a= 23;
+    if(a==23){
+    printf("b is equal to 23\n");
+    }
+    if(a>=20){
+    printf("b is greter than or equal to 20\n");
+    }
+    if(a<=25){
+    printf("b is less than or equal to 25\n");
+    }
+    if(a>20){
+    printf("b is greter than 20\n");
+    }
+    if(a<25){
+    printf("b is less than 25\n");
+    }
+    if(a!=25){
+    printf("b is not equal to 25\n");
+    }
+    
+    return 0;
+}
