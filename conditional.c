5293b68fb9660449e0696a8c991286b5607c05d6
Sudhanshu Verma <sudhanshu686verma@gmail.com>
2025-06-19 19:01:21 +0530

    conditional

---
+++ conditional.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int age=5;
+
+    if(age>10){
+        printf("your age is greater than 10\n");
+    }
+    // if(age%5==0){
+        // printf("your age is multiple of 5\n");
+    // }
+    else
+    printf("your age is less than 10\n");
+    return 0;
+}
